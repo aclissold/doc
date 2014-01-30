2027d46e28a68f7673603c76a3b6b81c4fef1fff
Andrew Clissold <andrewclissold@gmail.com>
2014-01-30 14:19:00 -0500

    Add Go, Python, and Google support

--- doc.go
+++ doc.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
+	"runtime"
+	"strings"
 )
 
 func main() {
-	exec.Command("xdg-open", "http://google.com/").Run()
+	open(combine(parse()))
+}
+
+// parse translates command-line arguments to a destination and parameters
+func parse() (string, string) {
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "usage: doc (go|py) (pkg|module)")
+		os.Exit(1)
+	}
+
+	return strings.ToLower(os.Args[1]), strings.Join(os.Args[2:], " ")
+}
+
+// combine builds a URL from a destination language and package/module/etc.
+func combine(lang, args string) string {
+	switch lang {
+	case "go", "golang":
+		return "http://golang.org/pkg/" + args + "/"
+	case "py", "python":
+		return "http://docs.python.org/3/library/" + args
+	default:
+		// Google it instead
+		return "https://www.google.com/search?q=" + args
+	}
+}
+
+// open launches a URL in the user's default Web browser
+func open(url string) {
+	switch runtime.GOOS {
+	case "linux":
+		exec.Command("xdg-open", url).Run()
+	case "darwin":
+		exec.Command("open", url).Run()
+	case "windows":
+		exec.Command("start", url).Run()
+	}
 }
